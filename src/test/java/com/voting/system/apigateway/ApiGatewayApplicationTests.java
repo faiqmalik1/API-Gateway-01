06a84b56c59d47c1f306ea8d082a4d1b44a2f424
Faiq Malik <faiq.malik@devsinc.com>
2023-08-24 12:56:06 +0500

    api gateway complete

---
+++ src/test/java/com/voting/system/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting.system.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
